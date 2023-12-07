67b6eb31b06f955c643442aac66ce2457c1306ce
Caia Chuang <caiachuang@gmail.com>
2023-12-07 10:19:55 -0800

    practice recursive binary search in python

--- 03_recursion/python/05_binary_search_recursive.py
+++ 03_recursion/python/05_binary_search_recursive.py
@@ -13,6 +13,26 @@ def binary_search(arr, target):
     else:
         return binary_search(arr[mid+1:], target)
 
-
+print("Answer")
 print(binary_search([6, 7, 8, 9, 10], 8))
 print(binary_search([6, 7, 8, 9, 10], 6))
+
+# Practice
+def bs_recursive(arr, target):
+    if not arr:
+        return -1
+
+    low = 0;
+    high = len(arr) - 1
+    mid = (low + high) // 2 # //: floor division
+
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return bs_recursive(arr[mid + 1:], target)
+    else:
+        return bs_recursive(arr[: mid], target)
+
+print("Practice")
+print(bs_recursive([6, 7, 8, 9, 10], 8))
+print(bs_recursive([6, 7, 8, 9, 10], 6))
